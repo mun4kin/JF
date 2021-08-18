fdae8e41d1388495f097199008640003aad37341
ArsBarsj <arsagulmsk@gmail.com>
2021-08-18 09:40:21 +0300

    feat: [components] тесты для Tile

--- src/components/atoms/Tile/Tile.spec.tsx
+++ src/components/atoms/Tile/Tile.spec.tsx
@@ -1,20 +1,24 @@
-import { shallow } from 'enzyme';
 import React from 'react';
+import { render } from '@testing-library/react';
 import Tile from './Tile';
 
 describe('Test <Tile/> component', () => {
+
+  it('should render Tile component', () => {
+    render(<Tile> Tile </Tile>)
+  });
+
   it('should have content with text "Content"', () => {
-    const wrapper = shallow(<Tile>
-      <p className='content'>Content</p>
-    </Tile>);
-    expect(wrapper.find('.content')).toHaveLength(1);
-    expect(wrapper.find('.content').text()).toBe('Content');
+    const { container } = render(<Tile><p className='content'>Content</p></Tile>);
+
+    expect(container.getElementsByClassName('content')).toHaveLength(1);
+    expect(container.querySelector('.content')).toHaveTextContent('Content');
   });
 
   it('should have class "custom-class"', () => {
-    const wrapper = shallow(<Tile className='custom-class'>
+    const { container } = render(<Tile className='custom-class'>
       <p className='content'>Content</p>
     </Tile>);
-    expect(wrapper.find('.rf-tile').hasClass('custom-class')).toBeTruthy();
+    expect(container.querySelector('.rf-tile')).toHaveClass('custom-class');
   });
 });
