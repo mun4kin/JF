190f9dc48855afc82d28398d8f772f61bd7c891f
Denis Yakshov <feerzlay@gmail.com>
2021-09-17 12:00:21 +0300

    feat: [input] add support for adornments

--- src/components/atoms/Input/Input.spec.tsx
+++ src/components/atoms/Input/Input.spec.tsx
@@ -1,10 +1,22 @@
-import { render } from '@testing-library/react';
 import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+
 import Input from './Input';
 
 describe('Test <Input/> component', () => {
   it('should have clear button', () => {
-    const { container } = render(<Input onClear={jest.fn} defaultValue='Test' />);
-    expect(container.getElementsByClassName('rf-input__action')).toHaveLength(1);
+    const onClear = jest.fn();
+    render(<Input onClear={onClear} defaultValue='Test' />);
+
+    expect(screen.queryByText('Сбросить')).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText('Сбросить'));
+    expect(onClear).toHaveBeenCalled();
   });
+
+  it('should render adornments', () => {
+    render(<Input defaultValue='Test' startAdornment={<div>start</div>} endAdornment={<div>end</div>}/>);
+    expect(screen.queryByText('start')).toBeInTheDocument();
+    expect(screen.queryByText('end')).toBeInTheDocument();
+  })
 });
