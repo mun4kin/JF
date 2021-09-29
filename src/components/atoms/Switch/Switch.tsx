f852629374de38e2f5e9eeeb15b97e0bdf47d0eb
Timofei Domnikov <timofei.d@softvoya.com>
2021-09-29 14:28:45 +0300

    feat [switch] align with design & update storybook

--- src/components/atoms/Switch/Switch.tsx
+++ src/components/atoms/Switch/Switch.tsx
@@ -7,39 +7,48 @@ import { Size } from '../../../types';
 export interface ISwitchProps {
   onChange?: (f: boolean) => void;
   label?: ReactNode;
+  helperText?: ReactNode;
   className?: string;
-  state?: boolean;
-  disable?: boolean;
+  value?: boolean;
+  disabled?: boolean;
   size?: Size;
+  style?: React.CSSProperties
 }
 
 const Switch: FC<ISwitchProps> = ({
   label,
+  helperText,
   className = '',
-  state = false,
-  disable = false,
+  value = false,
+  disabled = false,
   onChange,
-  size = 'm'
+  size = 'm',
+  style
 }: ISwitchProps) => {
-  const [s, toggle] = useState<boolean>(state);
+  const [s, toggle] = useState<boolean>(value);
 
   useEffect(() => {
-    toggle(state);
-  }, [state]);
+    toggle(value);
+  }, [value]);
 
   const changeState = () => {
-    if (!disable) {
+    if (!disabled) {
       onChange && onChange(!s);
       toggle(!s);
     }
   };
 
   return (
-    <div className={`rf-switch ${disable ? 'rf-switch--disable' : ''} rf-switch--${size} ${className}`} onClick={changeState}>
+    <div
+      className={`rf-switch ${disabled ? 'rf-switch--disable' : ''} rf-switch--${size} ${className}`}
+      onClick={changeState}
+      style={style}
+    >
       <div className={`rf-switch__toggle ${s ? 'on' : 'off'}`}>
         <div className='rf-switch__circle' />
       </div>
       {label && <p className='rf-switch__label'>{label}</p>}
+      {helperText && <p className='rf-switch__helper-text'>{helperText}</p>}
     </div>
   );
 };
