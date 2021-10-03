071077249b21abd3d9df4c13cbc9a02c58be25de
Michael Kutateladze <michael.kutateladze@yandex.ru>
2021-10-03 19:34:41 +0300

    segment

--- src/components/atoms/Segment/Segment.tsx
+++ src/components/atoms/Segment/Segment.tsx
@@ -93,7 +93,7 @@ const Segment: React.FC<ISegmentProps> = ({
 
     setActiveIndex(i);
     onChange(list[i]);
-  }, [setActiveIndex]);
+  }, [setActiveIndex, list]);
 
   // -------------------------------------------------------------------------------------------------------------------
 
